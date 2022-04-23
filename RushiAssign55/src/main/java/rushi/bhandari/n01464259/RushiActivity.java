a6122214662e27f5b64f1d179fb56872ecca6f4a
RushiBhandariAssign1 <n01464259@humbermail.com>
2022-04-23 14:45:21 -0400

    Initial commit

---
+++ RushiAssign55/src/main/java/rushi/bhandari/n01464259/RushiActivity.java
@@ -0,0 +1,14 @@
+package rushi.bhandari.n01464259;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RushiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
