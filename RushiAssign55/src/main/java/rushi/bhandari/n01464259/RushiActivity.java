1d8c9c7c1496a58ca4134e15fae645a9d96670e1
RushiBhandariAssign1 <n01464259@humbermail.com>
2022-04-23 15:21:38 -0400

    navigation drawer created

--- RushiAssign55/src/main/java/rushi/bhandari/n01464259/RushiActivity.java
+++ RushiAssign55/src/main/java/rushi/bhandari/n01464259/RushiActivity.java
@@ -1,13 +1,20 @@
 package rushi.bhandari.n01464259;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
 
-public class RushiActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class RushiActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     private Fragment homeFragment;
     private Fragment downloadFragment;
     private Fragment weatherFragment;
@@ -21,8 +28,37 @@ public class RushiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.configureToolBar();
+        this.configureDrawerLayout();
+        this.configurationNavigationView();
+
+        //Open Home fragement
+        Fragment fragment = HomeFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.rushiFrameLayout, fragment).commit();
 
 
 
+
+    }
+    private void configureToolBar() {
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void configureDrawerLayout() {
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navi_open,  R.string.navi_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configurationNavigationView() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
