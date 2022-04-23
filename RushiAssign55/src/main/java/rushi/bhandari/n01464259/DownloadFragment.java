8b2ab2d7158a8c7800b6f5b5d99cee3a2a071b2e
RushiBhandariAssign1 <n01464259@humbermail.com>
2022-04-23 16:11:28 -0400

    download fragment completed

--- RushiAssign55/src/main/java/rushi/bhandari/n01464259/DownloadFragment.java
+++ RushiAssign55/src/main/java/rushi/bhandari/n01464259/DownloadFragment.java
@@ -1,5 +1,9 @@
 package rushi.bhandari.n01464259;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +11,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +31,10 @@ import android.view.ViewGroup;
  */
 public class DownloadFragment extends Fragment {
 
+    View view = null;
+    ProgressBar progressBar;
+    ImageView imageView;
+    ProgressDialog p;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -56,7 +76,93 @@ public class DownloadFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_download, container, false);
+        View view = inflater.inflate(R.layout.fragment_download, container, false);
+
+        //Creating listview options
+        ListView listView = view.findViewById(R.id.rushiDownloadFragmentIdListView);
+
+        imageView = view.findViewById(R.id.rushiDownloadFragmentIdImageViewDisplay);
+
+        //Hiding progress bar
+        progressBar = view.findViewById(R.id.rushiDownloadFragmentIdProgressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        //Creating options
+        ArrayList<String> ImageOptions = new ArrayList<String>();
+        ImageOptions.add("Genius");
+        ImageOptions.add("Technology");
+        ImageOptions.add("Motivational Quotes");
+
+        //Setting array adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.list_view,ImageOptions);
+        listView.setAdapter(adapter);
+
+        //OnClick event handler
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = (String) adapterView.getItemAtPosition(i);
+                if(i == 0){
+                    AsyncTaskExample asyncTaskExample = new AsyncTaskExample();
+                    asyncTaskExample.execute("https://pepnewz.com/wp-content/uploads/Quiz-WHich-Type-of-Creative-Genius-Are-You_FB.png");
+
+                }else if(i == 1){
+                    AsyncTaskExample asyncTaskExample = new AsyncTaskExample();
+                    asyncTaskExample.execute("https://wallup.net/wp-content/uploads/2019/09/456778-computer-engineering-science-tech.jpg");
+
+                }else if(i ==2){
+                    AsyncTaskExample asyncTaskExample = new AsyncTaskExample();
+                    asyncTaskExample.execute("https://th.bing.com/th/id/OIP.MFOuZDMwALLbREUdt8fofAHaD8?pid=ImgDet&rs=1");
+                }
+            }
+        });
+        return view;
     }
+    //AsynTaskExample class to show image from internet
+    private class AsyncTaskExample extends AsyncTask<String,String,Bitmap> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmapImage = null;
+
+            try {
+                Thread.sleep(5000);
+                URL ImageUrl = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) ImageUrl.openConnection();
+
+                int responseCode = urlConnection.getResponseCode();
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream is = urlConnection.getInputStream();
+
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.RGB_565;
+                    bitmapImage = BitmapFactory.decodeStream(is, null,options);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            return bitmapImage;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap){
+            super.onPostExecute(bitmap);
+            if(imageView!= null){
+                progressBar.setVisibility(View.INVISIBLE);
+                imageView.setImageBitmap(bitmap);
+            }else {
+                p.show();
+            }
+
+        }
+    }
+
 }
