6fee3086b847f9fe54c213f0c8208f72b475dec0
RushiBhandariAssign1 <n01464259@humbermail.com>
2022-04-23 15:03:57 -0400

    fragments created

--- RushiAssign55/src/main/java/rushi/bhandari/n01464259/RushiActivity.java
+++ RushiAssign55/src/main/java/rushi/bhandari/n01464259/RushiActivity.java
@@ -1,14 +1,28 @@
 package rushi.bhandari.n01464259;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class RushiActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Fragment homeFragment;
+    private Fragment downloadFragment;
+    private Fragment weatherFragment;
+    private Fragment shapesFragment;
+    private Fragment filesFragment;
+    private Fragment settingsFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
