734f355096aab13d49934c98f6ba387451b351d3
RushiBhandariAssign1 <n01464259@humbermail.com>
2022-04-23 20:13:59 -0400

    storing data function completed in shape fragment

--- RushiAssign55/src/main/java/rushi/bhandari/n01464259/ShapesFragmenty.java
+++ RushiAssign55/src/main/java/rushi/bhandari/n01464259/ShapesFragmenty.java
@@ -2,14 +2,24 @@ package rushi.bhandari.n01464259;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +27,9 @@ import com.google.firebase.database.FirebaseDatabase;
  * create an instance of this fragment.
  */
 public class ShapesFragmenty extends Fragment {
-
+    Switch firebaseSwitch;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,10 +73,34 @@ public class ShapesFragmenty extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_shapes_fragmenty, container, false);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://rushiassign5-default-rtdb.firebaseio.com/");
-        DatabaseReference myRef = database.getReference("message");
+        AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.rushiShapesFragmentIdAutoCompleteView);
 
-        myRef.setValue("Hello, World!");
+        String[] autoCompleteTextEmailOptions = getResources().getStringArray(R.array.rushiAutoCompleteEmailArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,autoCompleteTextEmailOptions);
+
+         database = FirebaseDatabase.getInstance("https://rushiassign5-default-rtdb.firebaseio.com/");
+         myRef = database.getReference("Emails:");
+
+        firebaseSwitch = view.findViewById(R.id.rushiShapesFragmentIdFireDataBaseStorageEnableDisable);
+        firebaseSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(firebaseSwitch.isChecked() == true){
+                    Toast.makeText(getContext(),"Data storing On...",Toast.LENGTH_LONG).show();
+                    String autoTextData = autoCompleteTextView.getText().toString();
+                    myRef.setValue(autoTextData);
+                    autoCompleteTextView.setText("");
+                }
+            }
+        });
+
+
+
+
+
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(adapter);
+
 
 
 
